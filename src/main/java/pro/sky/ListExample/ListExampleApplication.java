6e69595c0e02944f7a794af5c52b7710b480ab4d
Unteryana <ishtar_v@bk.ru>
2023-06-15 14:46:33 +0300

    initial commit

---
+++ src/main/java/pro/sky/ListExample/ListExampleApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.ListExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListExampleApplication.class, args);
+	}
+
+}
